4db745b3a1d39151df9ddde9d647d9f2f87bceca
Ángel MB <114161450+Angelmbx@users.noreply.github.com>
2022-10-19 23:55:19 +0200

    Update B.java

--- src/B.java
+++ src/B.java
@@ -3,7 +3,6 @@ public class B {
     public static void main(String[] args) {
 
         if (args.length < 1) {
-            System.out.println("No se ha recibido ninguna cadena");
             System.exit(1);
         } else{
             System.exit(2);
